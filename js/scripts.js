af95bbf50a3ba8802f5264df89654d7f235a287d
IntelDoter <IntelDoter@gmail.com>
2016-07-29 23:53:17 +0500

    I got nothing to say

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,13 @@
  * Created by IntelDoter on 29.07.2016.
  */
 
-"use strict"
+"use strict";
 
+(function($){
+    $(function(){
 
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
