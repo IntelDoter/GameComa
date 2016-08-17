dc388c2b3cea1fe1f3ede1d90515f9ba3bf022d6
IntelDoter <IntelDoter@gmail.com>
2016-08-17 18:52:35 +0500

    v 0.3

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,16 @@
 "use strict";
 
 $(document).ready(function(){
-    $('.carousel').carousel();
+
+    var $angle_btn = $(".btn_angle");
+
+    $angle_btn.click(function() {
+       $angle_btn.css("transform","rotate(180deg)");
+    });
+
+    var options = [
+        {selector: '#server_list', offset: 0, callback: function() {
+            $angle_btn.css("transform","rotate(180deg)");
+        } }
+    ];
 });
