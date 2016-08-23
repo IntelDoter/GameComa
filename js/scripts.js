ebd9af172d8d57fdec0c8aeea005dbcdce971e5a
IntelDoter <IntelDoter@gmail.com>
2016-08-23 14:07:05 +0500

    v 0.6.1

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,16 @@
 
 $(document).ready(function(){
     $('.parallax').parallax();
-});
+
+    $('.caller').scrollfire({
+
+        offset: 0,
+        topOffset: 150,
+        bottomOffset: 150,
+
+        onTopHidden: function(nav_games){
+        $(elm).css("display", "block");
+    }
+    });
+
+})
