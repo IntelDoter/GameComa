9af6613ebbccadaa20f800a4440820493de27614
IntelDoter <IntelDoter@gmail.com>
2016-10-04 19:31:34 +0500

    v 0.8

--- js/scripts.js
+++ js/scripts.js
@@ -8,12 +8,40 @@ $(document).ready(function(){
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy();
 
-    $('#lightSlider').lightSlider({
-        gallery: true,
-        item: 1,
-        loop:true,
-        slideMargin: 0,
-        thumbItem: 9
+    /* slider */
+    $('.next').click(function() {
+        var current = $('.current');
+        var parent = current.parent();
+        if (current.next().is("li")) {
+            var nextEl = current.next();
+            current.removeClass('current');
+            nextEl.addClass('current');
+        } else {
+            current.removeClass('current');
+            parent.children().first().addClass('current');
+        }
+    });
+    $('.back').click(function() {
+        var current = $('.current');
+        var parent = current.parent();
+        if (current.prev().is("li")) {
+            var nextEl = current.prev();
+            current.removeClass('current');
+            nextEl.addClass('current');
+        } else {
+            current.removeClass('current');
+            parent.children().last().addClass('current');
+        }
+    });
+
+    $(".play").click(function() {
+        var elem = $(this).parent().parent().find(".play_action");
+        elem.css("display", "block");
+    });
+
+    $(".close_action").click(function() {
+        var elem = $(this).parent();
+        elem.css("display", "none");
     });
 
 });
